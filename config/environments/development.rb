afd9b2a27f2d70179394a16881383630af656d28
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2016-11-15 18:13:09 +0100

    Added machinist gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,11 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.generators do |g|
+    g.stylesheets = false
+    g.javascripts = false
+    g.helper = false
+    g.test_unit = { fixture_replacement: :machinist }
+  end
 end

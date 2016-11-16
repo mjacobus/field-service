fe9883d1dd7379b0c4c16cc0072fa80c7c5c6933
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2016-11-16 16:53:45 +0100

    Added householder validation

--- app/models/householder.rb
+++ app/models/householder.rb
@@ -1,3 +1,7 @@
 class Householder < ApplicationRecord
   belongs_to :territory
+
+  validates :name, presence: true
+  validates :street_name, presence: true
+  validates :house_number, presence: true
 end
--- test/models/householder_test.rb
+++ test/models/householder_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class HouseholderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def valid_record
+    Householder.make
+  end
+
+  test "valid record is considered valid" do
+    assert valid_record.valid?
+  end
+
+  test "validates presence of #street_name" do
+    record = valid_record
+    record.street_name = nil
+
+    assert !record.valid?
+  end
+
+  test "validates presence of #name" do
+    record = valid_record
+    record.name = nil
+
+    assert !record.valid?
+  end
+
+  test "validates presence of #house_number" do
+    record = valid_record
+    record.house_number = nil
+
+    assert !record.valid?
+  end
 end

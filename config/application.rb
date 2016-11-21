70608b1154c9529fd45ef2ee0b71fa19f050c2a5
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2016-11-21 20:10:07 +0100

    Renamed view_objects folder to decorators

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,5 @@ module FieldService
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
-    config.autoload_paths << "#{Rails.root}/view_objects"
   end
 end

95d4be96f8debbbbe0ff9d477d722a1c62c4404b
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2016-12-14 11:30:32 -0200

    Added partial pt-BR translations

--- config/application.rb
+++ config/application.rb
@@ -13,5 +13,7 @@ module FieldService
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.autoload_paths << Rails.root.join('lib')
+
+    config.i18n.default_locale = 'pt-BR'
   end
 end

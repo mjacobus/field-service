e9cc60fe727824dba73857c8315ccfe961790dbe
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2016-12-29 12:54:10 -0200

    Better icons

--- app/decorators/active_record_base_decorator.rb
+++ app/decorators/active_record_base_decorator.rb
@@ -8,9 +8,16 @@ class ActiveRecordBaseDecorator < BaseDecorator
   # http://zurb.com/playground/foundation-icon-fonts-3
   def button_link_with_icon(url, icon, button_type = nil, options = {})
     options = options.merge(class: "button #{button_type}")
+    content = content_tag(:i, nil, class: "fi-#{icon}")
 
-    link_to url, options do
-      content_tag(:i, nil, class: "fi-#{icon}")
+    if options[:text]
+      text = content_tag(:div, options[:text])
+
+      content = content_tag(:span) do
+         content + text
+      end
     end
+
+    link_to(url, options) { content }
   end
 end

63764c9c45b5839f3aa72e24a96bf33dcbfa1c0d
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2017-05-08 20:37:02 +0200

    Added unique id class

---
+++ test/models/unique_id_test.rb
@@ -0,0 +1,33 @@
+require "test_helper"
+
+class UniqueIdTest < TestCase
+  test 'when no id is given in the constructor creates a new unique id' do
+    SecureRandom.expects(:uuid).returns('foobar')
+
+    assert_equal 'foobar', UniqueId.new.to_s
+  end
+
+  test 'when id is passed in the constructor uses that value as unique id' do
+    string = UniqueId.new('foo').to_s
+
+    assert_equal 'foo', string
+  end
+
+  test '#== returns true when values are the same' do
+    assert_equal UniqueId.new('foo'), UniqueId.new('foo')
+  end
+
+  test '#== returns false when values are different' do
+    assert_not_equal UniqueId.new('foo'), UniqueId.new('bar')
+  end
+
+  test 'it is immutable' do
+    value = 'foo'
+
+    id = UniqueId.new(value)
+
+    value.upcase!
+
+    assert_equal 'foo', id.to_s
+  end
+end

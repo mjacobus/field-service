4b0b68bf53628b8890749834b14715ac5d2877cb
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2017-05-16 21:10:43 +0200

    WIP

--- app/services/csv_importer.rb
+++ app/services/csv_importer.rb
@@ -11,9 +11,31 @@ class CsvImporter
 
   def import(file)
     contents = File.read(file)
-    ap contents
-    collection = @parser.parse(contents).map(&:symbolize_keys)
-    ap collection
+    filter = Filter.new
+    collection = @parser.parse(contents).map do |row|
+      row = filter.filter(row)
+      ap row
+    end
     @importer.import_batch(collection)
+  end
+
+  class Filter
+    ALLOWED_KEYS = %i[
+      territory_name
+      street_name
+      house_number
+      name
+      show
+      uuid
+      updated_at
+    ].freeze
+
+    def filter(input)
+      input.dup.symbolize_keys.tap do |filtered|
+        filtered.keys.each do |key|
+          filtered.delete(key) unless ALLOWED_KEYS.include?(key)
+        end
+      end
+    end
   end
 end

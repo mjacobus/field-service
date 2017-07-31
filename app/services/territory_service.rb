9eb33ffd3e049d8e89f8261b3332e8fa5b43bf5b
Marcelo Jacobus <marcelo.jacobus@xing.com>
2017-07-31 17:51:12 +0200

    Add service for filtering territories

--- app/services/territory_service.rb
+++ app/services/territory_service.rb
@@ -1,2 +1,40 @@
 class TerritoryService
+  def search(assigned_to_ids: nil)
+    query = TerritoryQuery.new
+
+    if assigned_to_ids
+      query = query.with_publisher_ids(assigned_to_ids)
+    end
+
+    query
+  end
+end
+
+class TerritoryQuery < SimpleDelegator
+  def initialize(scope = Territory.sorted)
+    super(scope)
+  end
+
+  def with_publisher_ids(ids)
+    new_scope = assigned.where(territory_assignments: { publisher_id: array_of_ints(ids) })
+    new(new_scope)
+  end
+
+  def assigned
+    new(joins(:assignments).where(territory_assignments: { returned_at: nil }))
+  end
+
+  private
+
+  def new(scope)
+    self.class.new(scope)
+  end
+
+  def scope
+    __item__
+  end
+
+  def array_of_ints(items)
+    items.map { |item| Integer(item) }
+  end
 end
---
+++ spec/services/territory_service_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+RSpec.describe TerritoryService do
+  let(:territory1) { Territory.make! }
+  let(:territory2) { Territory.make! }
+  let(:territory3) { Territory.make! }
+
+  let(:publisher1) { Publisher.make! }
+  let(:publisher2) { Publisher.make! }
+
+  describe '#search' do
+    describe 'with no parameters' do
+      before do
+        territory1
+      end
+
+      it 'returns all records' do
+        results = TerritoryService.new.search
+
+        expect(results.count).to eq(1)
+        expect(results.to_a).to eq([territory1])
+      end
+    end
+
+    describe 'with assigned_to_ids' do
+      before do
+        TerritoryAssigning.new.perform(
+          territory_id: territory1.id,
+          publisher_id: publisher1.id
+        )
+
+        TerritoryAssigning.new.perform(
+          territory_id: territory1.id,
+          publisher_id: publisher2.id
+        )
+
+        TerritoryAssigning.new.perform(
+          territory_id: territory2.id,
+          publisher_id: publisher2.id
+        )
+
+        TerritoryAssigning.new.perform(
+          territory_id: territory3.id,
+          publisher_id: publisher1.id
+        )
+      end
+
+      it 'returns only the territories assigned to the given ids' do
+        results = TerritoryService.new.search(assigned_to_ids: [publisher1.id])
+
+        expect(results.count).to eq(1)
+        expect(results.to_a).to eq([territory3])
+      end
+    end
+  end
+end

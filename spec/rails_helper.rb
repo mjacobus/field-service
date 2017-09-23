8b82bb802a1e8b771632a4b51c9f151580d9676c
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2017-09-23 22:10:00 +0200

    Refactor test configs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,32 +1,9 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-ENV['RAILS_ENV'] ||= 'test'
-
-FIXTURES_PATH = File.expand_path('../fixtures/', __FILE__)
-
-if ENV['COVERAGE']
-  require 'simplecov'
-  require 'coveralls'
-
-  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
-                                                                   SimpleCov::Formatter::HTMLFormatter,
-                                                                   Coveralls::SimpleCov::Formatter
-                                                                 ])
-
-  SimpleCov.start 'rails' do
-    add_filter 'app/channels/application_cable/channel.rb'
-    add_filter 'app/channels/application_cable/connection.rb'
-    add_filter 'app/jobs/application_job.rb'
-    add_filter 'app/mailers/application_mailer.rb'
-    add_group 'Decorators', 'app/decorators'
-  end
-end
-
-require File.expand_path('../../config/environment', __FILE__)
-# Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'common'
 
 require 'rspec/rails'
+
 require File.expand_path(File.dirname(__FILE__) + '/support/blueprints')
 
 # Add additional requires below this line. Rails is not loaded until this point!

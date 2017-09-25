3921da05c8d438a56fb74bd35916f3b578075b2f
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2017-09-25 22:04:25 +0200

    Cleanup

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,58 +1,45 @@
+ENV['RAILS_ENV'] ||= 'test'
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-require 'common'
+
+if ENV['COVERAGE']
+  require 'simplecov'
+  require 'coveralls'
+
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
+    [
+      SimpleCov::Formatter::HTMLFormatter,
+      Coveralls::SimpleCov::Formatter
+    ]
+  )
+
+  SimpleCov.start 'rails' do
+    add_filter 'app/channels/application_cable/channel.rb'
+    add_filter 'app/channels/application_cable/connection.rb'
+    add_filter 'app/jobs/application_job.rb'
+    add_filter 'app/mailers/application_mailer.rb'
+    add_group 'Decorators', 'app/decorators'
+    add_group 'Services', 'app/services'
+  end
+end
+
+FIXTURES_PATH = File.expand_path('../fixtures/', __FILE__)
+
+require File.expand_path('../../config/environment', __FILE__)
+
+# Prevent database truncation if the environment is production
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 
 require 'rspec/rails'
 require "clearance/rspec"
 
 require File.expand_path(File.dirname(__FILE__) + '/support/blueprints')
-
-# Add additional requires below this line. Rails is not loaded until this point!
-
-# Requires supporting ruby files with custom matchers and macros, etc, in
-# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
-# run as spec files by default. This means that files in spec/support that end
-# in _spec.rb will both be required and run as specs, causing the specs to be
-# run twice. It is recommended that you do not name files matching this glob to
-# end with _spec.rb. You can configure this pattern with the --pattern
-# option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
-#
-# The following line is provided for convenience purposes. It has the downside
-# of increasing the boot-up time by auto-requiring all files in the support
-# directory. Alternatively, in the individual `*_spec.rb` files, manually
-# require only the support files necessary.
-#
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
-
-# Checks for pending migration and applies them before tests are run.
-# If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
   config.use_transactional_fixtures = true
-
-  # RSpec Rails can automatically mix in different behaviours to your tests
-  # based on their file location, for example enabling you to call `get` and
-  # `post` in specs under `spec/controllers`.
-  #
-  # You can disable this behaviour by removing the line below, and instead
-  # explicitly tag your specs with their type, e.g.:
-  #
-  #     RSpec.describe UsersController, :type => :controller do
-  #       # ...
-  #     end
-  #
-  # The different available types are documented in the features, such as in
-  # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
-
-  # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
@@ -60,4 +47,31 @@ end
 
 def t(*args)
   I18n.t(*args)
+end
+
+class FakeViewHelpers
+  def t(arg)
+    ['t', arg].join('.')
+  end
+end
+
+def assert_delegates(method, delegator, delegated)
+  message = "does not delegate #{method} to #{delegated}"
+
+  assert_respond_to delegator, method, message
+  assert_respond_to delegated, method, "delegated does not respond to #{method}"
+
+  assert_equal(
+    delegator.send(method),
+    delegated.send(method),
+    message
+  )
+
+  assert_equal(
+    delegator.send(method),
+    delegated.send(method),
+    message
+  )
+
+  expect(delegated.send(method)).not_to(be_nil, 'cannot check if delegates if value is nil')
 end

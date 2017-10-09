d6801320e01bea532ce9770e40162cdfc388cca9
Marcelo Jacobus <marcelo.jacobus@xing.com>
2017-10-09 15:28:12 +0200

    Add basic territory list view

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import TerritoryList from './views/pages/territories/index';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {BrowserRouter, Route} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const app =  <BrowserRouter>
+                <Route path="/frontend" component={TerritoryList}/>
+             </BrowserRouter>;
+
+ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();

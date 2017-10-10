c589f8150b4deb3758cefa8bf1dc94b58ac365f7
Marcelo Jacobus <marcelo.jacobus@xing.com>
2017-10-10 14:44:54 +0200

    Add loader after refresh is clicked

--- client/src/actions/territory-actions.js
+++ client/src/actions/territory-actions.js
@@ -1,16 +1,21 @@
 import Ajax from '../utils/ajax-helper';
 
-export const UPDATE_TERRITORIES = 'UPDATE_TERRITORIES';
+export const TERRITORIES_UPDATE_ITEMS = 'TERRITORIES_UPDATE_ITEMS';
+export const TERRITORIES_FETCH_REQUEST = 'TERRITORIES_FETCH_REQUEST';
 
 export function updateTerritoriesResult(territories) {
   return {
-    type: UPDATE_TERRITORIES,
+    type: TERRITORIES_UPDATE_ITEMS,
     territories
   };
 };
 
 export function fetchTerritories() {
   return dispatch => {
+    dispatch({
+      type: TERRITORIES_FETCH_REQUEST
+    });
+
     Ajax.getJson('/api/territories').then(response => {
       dispatch(updateTerritoriesResult(response.data));
     });

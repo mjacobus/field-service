3acd7e0d205a59d4a6d5aea8db3803122822a671
Marcelo Jacobus <marcelo.jacobus@xing.com>
2017-10-11 16:52:21 +0200

    Rename file territories/{list => territory-list}.js

--- client/src/containers/pages/territories/list-container.js
+++ client/src/containers/pages/territories/list-container.js
@@ -1,5 +1,5 @@
 import {connect} from 'react-redux';
-import TerritoryList from '../../../views/pages/territories/list';
+import TerritoryList from '../../../views/pages/territories/territory-list';
 import { fetchTerritories } from '../../../actions/territory-actions';
 
 function mapStateToProps(state) {

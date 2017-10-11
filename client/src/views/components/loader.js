6deb5618da1de7585d5b9cad3a410146e7fce1e7
Marcelo Jacobus <marcelo.jacobus@xing.com>
2017-10-11 11:57:38 +0200

    Enhance loader with a spinner

--- client/src/views/components/loader.js
+++ client/src/views/components/loader.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import loaderImage from './assets/loader.gif';
+import styles from './loader.css';
 
 class Loader extends Component {
-  static defaultProps = { loading: true };
+  static defaultProps = {
+    loading: true,
+    size: 50,
+  };
 
   render() {
     return (
-      <p>Loading...</p>
+      <div className={styles.loader}>
+        <img height={this.props.size} width={this.props.size}src={loaderImage} alt="Loading..."/>
+      </div>
     );
   }
 }

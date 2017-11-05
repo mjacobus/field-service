ef54762c8e6598ebf7806d84a10745278b1a83e1
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2017-11-05 21:53:55 +0100

    Improve design

--- client/src/views/pages/shared/householder-item.js
+++ client/src/views/pages/shared/householder-item.js
@@ -29,23 +29,27 @@ export default ({ householder, onDelete = noop, separator = true }) => {
     <div className={classNames.join(' ')}>
       <Row>
         <Col xs={8}>
+          <Row>
             <Col xs={12} md={7}>
               <Item description={ t.address } separator={ false }>{ householderEditLink(householder.address) }</Item>
             </Col>
             <Col xs={12} md={5}>
               <Item description={ t.name } separator={ false }>{ householderEditLink(householder.name) }</Item>
             </Col>
+          </Row>
         </Col>
         <Col xs={4}>
-          <Col xs={12} md={6}>
-            {
-              householder.doNotVisitDate &&
-              <Item description={ t.doNotVisit } separator={ false }><Date>{ householder.doNotVisitDate }</Date></Item>
-            }
-          </Col>
-          <Col xs={12} md={6}>
-            <DeleteHouseholderButton householder={ householder } onDelete={ onDelete } className={ styles.deleteButton } />
-          </Col>
+          <Row>
+            <Col xs={12} md={6}>
+              {
+                householder.doNotVisitDate &&
+                <Item className={ styles.floatRight } description={ t.doNotVisit } separator={ false }><Date>{ householder.doNotVisitDate }</Date></Item>
+              }
+            </Col>
+            <Col xs={12} md={6}>
+              <DeleteHouseholderButton householder={ householder } onDelete={ onDelete } className={ styles.deleteButton } />
+            </Col>
+          </Row>
         </Col>
       </Row>
 

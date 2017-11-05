8ee835de4d22ccf0dccdf81bf913d165c68014d2
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2017-11-05 17:14:54 +0100

    Refactor

--- client/src/views/components/form.js
+++ client/src/views/components/form.js
@@ -1,26 +1,12 @@
 import React from 'react'
 
-const getCookie = (name) => {
-  const re = new RegExp(name + "=([^;]+)");
-  let value = re.exec(document.cookie);
-  return (value != null) ? unescape(value[1]) : null;
-}
-
-const token = () => {
-  let tokenValue = (document.querySelector('[name="csrf-token"]') || {}).content || null;
-
-  if (tokenValue === null) {
-    tokenValue = getCookie('fs_form_token');
-  }
-
-  return tokenValue;
-};
+import cookies from '../../utils/cookies.js'
 
 export default ({ action = '', method = 'post', children }) => {
   return <form action={action} method="post">
     <input name="utf8" type="hidden" value="✓" />
     <input type="hidden" name="_method" value={ method }/>
-    <input type="hidden" name="authenticity_token" value={ token() }/>
+    <input type="hidden" name="authenticity_token" value={ cookies.getFormToken() }/>
     { children }
   </form>;
 };

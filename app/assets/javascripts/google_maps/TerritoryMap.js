ca731a5904c650f5d2a9c2f1f2f025f17ad18bda
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2018-02-21 12:19:02 +0100

    Add important events

--- app/assets/javascripts/google_maps/TerritoryMap.js
+++ app/assets/javascripts/google_maps/TerritoryMap.js
@@ -5,6 +5,7 @@
 // https://developers.google.com/maps/documentation/javascript/examples/polygon-hole
 // https://developers.google.com/maps/documentation/javascript/examples/polygon-arrays
 // https://developers.google.com/maps/documentation/javascript/examples/control-custom
+// https://gist.github.com/christophercliff/1380958
 
 class TerritoryMap {
   static draw({ container, territory }) {
@@ -21,26 +22,12 @@ class TerritoryMap {
   constructor({ territory, container, map }) {
     this.getMarkers = this._getMarkers.bind(this);
     this.centralize = this.centralize.bind(this);
+    this.drawEditor = this.drawEditor.bind(this);
+    this.loadPolygon = this.loadPolygon.bind(this);
 
     this.map = map;
     this.territory = territory;
     this.markers = this._getMarkers();
-  }
-
-  centralize() {
-    // sum of lat/lon
-    const sum = this.markers.reduce((accumulator, marker) => {
-      accumulator.lat += marker.position.lat;
-      accumulator.lng += marker.position.lon;
-
-      return accumulator;
-    }, { lat: 0, lng: 0 });
-
-    // average of lat/lon
-    const length = this.markers.length;
-    const average = { lat: sum.lat/length, lng: sum.lng/length };
-
-    this.map.setCenter(average);
   }
 
   draw() {
@@ -57,7 +44,8 @@ class TerritoryMap {
       });
     });
 
-    this._drawPolygon();
+    this.drawEditor();
+    this.loadPolygon();
   }
 
   _getMarkers() {
@@ -76,33 +64,43 @@ class TerritoryMap {
     });
   }
 
-  _drawPolygon() {
+  drawEditor() {
     const drawingManager = new google.maps.drawing.DrawingManager({
       drawingControl: true,
       drawingControlOptions: {
         position: google.maps.ControlPosition.TOP_CENTER,
         drawingModes: ['polygon']
       },
-      circleOptions: {
+      polygonOptions: {
         fillColor: null,
         fillOpacity: 0,
-        strokeWeight: 5,
+        strokeWeight: 2,
         clickable: false,
-        editable: true,
+        editable: !true,
         zIndex: 1
       }
     });
 
-    drawingManager.setMap(this.map);
+    // drawingManager.setMap(this.map);
 
-    google.maps.event.addListener(drawingManager, 'polygoncomplete', (poly) => {
-      const coordinates = [];
-      poly.getPath().forEach((loc) => coordinates.push({ lat: loc.lat(), lng: loc.lng() }));
-      console.log(coordinates);
-    });
 
+    google.maps.event.addListener(drawingManager, 'polygoncomplete', this.logCoordenates);
+  }
+
+  logCoordenates(poly) {
+    const coordinates = [];
+    poly.getPath().forEach((loc) => coordinates.push({ lat: loc.lat(), lng: loc.lng() }));
+    console.log(coordinates);
+  }
+
+  loadPolygon() {
     // Define the LatLng coordinates for the polygon.
-    const coordenates = this._savedMap();
+    const coordenates = [
+      { lat: 53.55050557090095, lng: 9.994030470886173 },
+      { lat: 53.54823631218663, lng: 9.995489592590275 },
+      { lat: 53.54917972665483, lng: 10.003171439208927 },
+      { lat: 53.55065855025642, lng: 10.00209855560297 }
+    ];
 
     // Construct the polygon.
     const savedTerritory = new google.maps.Polygon({
@@ -115,15 +113,37 @@ class TerritoryMap {
       fillOpacity: 0
     });
     savedTerritory.setMap(this.map);
+
+    google.maps.event.addListener(savedTerritory.getPath(), 'set_at', () => {
+      console.log('set_at');
+      this.logCoordenates(savedTerritory);
+    });
+
+    google.maps.event.addListener(savedTerritory.getPath(), 'insert_at', () => {
+      console.log('insert_at');
+      this.logCoordenates(savedTerritory);
+    });
+
+    google.maps.event.addListener(savedTerritory.getPath(), 'remove_at', () => {
+      console.log('remove_at');
+      this.logCoordenates(savedTerritory);
+    });
   }
 
-  _savedMap() {
-    return [
-      { lat: 53.55050557090095, lng: 9.994030470886173 },
-      { lat: 53.54823631218663, lng: 9.995489592590275 },
-      { lat: 53.54917972665483, lng: 10.003171439208927 },
-      { lat: 53.55065855025642, lng: 10.00209855560297 }
-    ];
+  centralize() {
+    // sum of lat/lon
+    const sum = this.markers.reduce((accumulator, marker) => {
+      accumulator.lat += marker.position.lat;
+      accumulator.lng += marker.position.lon;
+
+      return accumulator;
+    }, { lat: 0, lng: 0 });
+
+    // average of lat/lon
+    const length = this.markers.length;
+    const average = { lat: sum.lat/length, lng: sum.lng/length };
+
+    this.map.setCenter(average);
   }
 
 }

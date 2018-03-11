6b81c4ee36873f896b2d8b0956fd97ab7333c9f5
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2018-03-11 23:02:09 +0100

    Small refactory

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,28 +6,9 @@ class ProfilesController < AuthenticatedController
   end
 
   def update
-    password = user_params[:password].presence
-    new_password = user_params[:new_password].presence
-
-    user = current_user
-
-    user.email = user_params[:email]
-    user.valid?
-
-    if new_password
-      unless user.authenticated?(password)
-        user.errors.add(:password, :invalid)
-      end
-
-      user.password = new_password
-    end
-
-    if user.errors.empty?
-      user.save!
-      redirect_to territories_path
-      return
-    end
-
+    User.update_profile(current_user, user_params)
+    redirect_to territories_path
+  rescue User::ValidationError => user
     @user = user
     render :edit
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
+  class ValidationError < RuntimeError
+    attr_reader :user
+    def initialize(user)
+      super('Invalid user')
+      @user = user
+    end
+  end
+
   include Clearance::User
 
   def territories
@@ -8,5 +16,30 @@ class User < ApplicationRecord
 
     publisher_ids = UserPublisher.where(user_id: id).select(:publisher_id)
     Territory.where(responsible_id: publisher_ids)
+  end
+
+  # TODO: quick code. Need to be extracted into a descent service
+  def self.update_profile(user, attributes)
+    password = attributes[:password].presence
+    new_password = attributes[:new_password].presence
+    user.email = attributes[:email]
+
+    unless user.valid?
+      raise ValidationError, user
+    end
+
+    if new_password
+      unless user.authenticated?(password)
+        user.errors.add(:password, :invalid)
+      end
+
+      user.password = new_password
+    end
+
+    unless user.errors.empty?
+      raise ValidationError, user
+    end
+
+    user.save!
   end
 end
